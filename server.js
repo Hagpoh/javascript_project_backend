222648191cef61e854fab72bcd347c4804ad2440
Meebly <hagpoh@gmail.com>
2022-06-14 13:00:41 -0700

    added API and Heroku functionality to the backend

--- server.js
+++ server.js
@@ -1,31 +1,18 @@
-const { response } = require('express')
 const express = require('express')
-const fetch = require('node-fetch')
-
+const cors = require('cors')
+const dataHandler = require('./helpers')
 const app = express()
+app.use(cors())
 
-app.listen(3000, () => {
+let PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
   console.log('Listening on port 3000')
 })
 
-app.get('/', (req, res) => {
-  // fetch('http://www.thecocktaildb.com/api/json/v1/1/random.php')
-  //   .then(response => response.json())
-  //   .then(data => res.send(data))
+// localhost:3000?loc=fresno
+app.get('/', async (req, res) => {
+  const { loc } = req.query
+  const data = await dataHandler.getData(loc)
+  res.send(data)
 })
-
-fetch('http://www.thecocktaildb.com/api/json/v1/1/random.php')
-  .then(response => response.json())
-  .then(data => console.log(data))
-
-fetch(
-  'https://api.openweathermap.org/data/2.5/weather?lat=36&lon=119&appid=9eb3d733e0c3e1c9eca3820f3a53f491'
-)
-  .then(response => response.json())
-  .then(data => console.log(data))
-
-fetch(
-  'https://api.opencagedata.com/geocode/v1/json?q=fresno california&key=b2e6eee44efa4813bb50007249307c34'
-)
-  .then(response => response.json())
-  .then(data => console.log(data))
