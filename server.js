e02ff12cce96585e2a3ce3a6ef5b43748ce0cc72
Meebly <hagpoh@gmail.com>
2022-06-15 10:21:12 -0700

    added database post and get functionality

--- server.js
+++ server.js
@@ -3,24 +3,40 @@ const cors = require('cors')
 const dataHandler = require('./helpers')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
-app.use(cors())
 
-const connectionString = "mongodb+srv://Cocktail:drinkrecommendations@cocktailapp.fve6u.mongodb.net/?retryWrites=true&w=majority"
+const connectionString =
+  'mongodb+srv://Cocktail:drinkrecommendations@cocktailapp.fve6u.mongodb.net/?retryWrites=true&w=majority'
 
 let PORT = process.env.PORT || 3000
 
-MongoClient.connect(connectionString, (err, client) => {
-  if (err) return console.error(err)
-  console.log('Connected to Database')
-})
+MongoClient.connect(connectionString).then(client => {
+  const db = client.db('cocktail-recommendation-history')
+  const cocktailCollection = db.collection('cocktails')
 
-app.listen(PORT, () => {
-  console.log('Listening on port: ', PORT)
-})
+  app.use(cors())
 
-// localhost:3000?loc=fresno
-app.get('/', async (req, res) => {
-  const { loc } = req.query
-  const data = await dataHandler.getData(loc)
-  res.send(data)
+  app.listen(PORT, () => {
+    console.log('Listening on port: ', PORT)
+  })
+
+  // localhost:3000/postDrink?loc=fresno&name=pat
+  app.post('/postDrink', async (req, res) => {
+    const { loc, name } = req.query
+    const data = await dataHandler.getData(loc)
+    await cocktailCollection.insertOne({
+      fName: name,
+      ...data
+    })
+    res.send(data)
+  })
+
+  // localhost:3000/getAllDrinks
+  app.get('/getAllDrinks', (req, res) => {
+    cocktailCollection
+      .find()
+      .toArray()
+      .then(results => {
+        res.send(results)
+      })
+  })
 })
